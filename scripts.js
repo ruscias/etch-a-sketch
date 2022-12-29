429691652e9b7b602ba5e8a079a8725e17480359
ruscias <stevenwrusciano@gmail.com>
2022-12-29 17:29:56 -0600

    Add logic to change multiple divs color on mouse over

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const colorPicker = document.querySelector('input[type="color"]');
 
-const testDiv = document.querySelector('div#test-div');
+const allDrawingDivs = document.querySelectorAll('div.drawing-div');
 
 // need a default color... I think...
 let currentColor = 'red';
@@ -8,16 +8,18 @@ let currentColor = 'red';
 // function to be called when value in color picker is changed  
 function watchColorPicker(event) {
   currentColor = event.target.value;
+  console.log(currentColor);
 }
 
 // function to be called on mouse over event in a div
 function changeColor() {
-  console.log(currentColor)
-  testDiv.style.backgroundColor = currentColor;
+  this.style.backgroundColor = currentColor;
 }
 
 // actual event listener looking for changes to color picker
 colorPicker.addEventListener('change', watchColorPicker);
 
-// event listener looking for mouse over events in div#test-div
-testDiv.addEventListener('mouseover', changeColor);
+
+allDrawingDivs.forEach( (drawDiv) => {
+  drawDiv.addEventListener('mouseover',changeColor)
+});
