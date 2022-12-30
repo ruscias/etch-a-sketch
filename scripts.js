591dbb77d4e4dc3f7a3b7372ae4939ae35c49d99
ruscias <stevenwrusciano@gmail.com>
2022-12-29 18:19:48 -0600

    Test changing color of dynamically generated divs

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
+var columns = 3;
+var rows = 4;
+
+var grid = document.createElement('div');
+grid.className = 'grid';
+for (var i = 0; i < columns; ++i) {
+  var column = document.createElement('div'); // create column
+  column.className = 'column';
+  for (var j = 0; j < rows; ++j) {
+    var row = document.createElement('div'); // create row
+    row.className = 'row';
+    row.textContent = i + '-' +j; // set text
+    column.appendChild(row); // append row in column
+  }
+  grid.appendChild(column); // append column inside grid
+}
+document.body.appendChild(grid);
+
 const colorPicker = document.querySelector('input[type="color"]');
 
-const allDrawingDivs = document.querySelectorAll('div.drawing-div');
+const allDrawingDivs = document.querySelectorAll('div.row');
 
 // need a default color... I think...
 let currentColor = 'red';
